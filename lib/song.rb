40840eedf593aee2bac815a3b92c137a553d4420
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-31 15:06:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,47 @@ class Song
     self.class.all << self
   end
 
+  # Class methods
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_title)
+    song = self.new
+    song.name = song_title
+    song
+  end
+
+  def self.create_by_name(song_title)
+    song = self.create
+    song.name = song_title
+    song
+  end
+
+  def self.find_by_name(song_title)
+    @@all.find {|song| song.name == song_title}
+  end
+
+  def self.find_or_create_by_name(song_title)
+
+  end
+
+  def self.alphabetical
+
+  end
+
+  def self.new_from_filename
+
+  end
+
+  def self.create_from_filename
+
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
